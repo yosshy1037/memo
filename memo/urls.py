fa35ab91956a993fe57918cda206dde82b721508
yosshy_git1037 <arsenal1037@gmail.com>
2019-08-09 15:40:49 +0900

    second commit.

--- memo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .memoSearch import views,resultListPost
-from . import postgres
-
-urlpatterns = [
-    url(r'^memo/$', views.memo, name='memo'),
-    url(r'^dataList/$', resultListPost.resultListView, name='resultListView'),
-    url(r'^postgres/$', postgres.db_test, name='db_test'),
-]
