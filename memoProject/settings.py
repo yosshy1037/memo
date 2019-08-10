b075f91bccba8739738081852e1414e0053f393a
yosshy_git1037 <arsenal1037@gmail.com>
2019-08-10 22:45:51 +0900

    make it better

--- memoProject/settings.py
+++ memoProject/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'memoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'memo',
-        'USER': 'admin',
-        'PASSWORD': 'kanai',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'memo',
+#        'USER': 'admin',
+#        'PASSWORD': 'kanai',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432,
+#    }
+#}
 
 # update db settings
 import dj_database_url
